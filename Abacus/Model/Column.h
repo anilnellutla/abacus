fab3953c8ae7a0ee1de41c4f4eaf25e271bbe1ba
Anil K Nellutla <anil@anils-mbp-2.home>
2014-03-24 21:58:44 -0400

    no message

--- Abacus/Model/Column.h
+++ Abacus/Model/Column.h
@@ -1,13 +1,13 @@
 //
-//  Bead.h
+//  Column.h
 //  Abacus
 //
-//  Created by Anil K Nellutla on 3/17/14.
+//  Created by Anil K Nellutla on 3/24/14.
 //  Copyright (c) 2014 Anil K Nellutla. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface Bead : NSObject
-@property (nonatomic) NSUInteger value;
+@interface Column : NSObject
+
 @end
---
+++ Abacus/Model/Column.m
@@ -0,0 +1,13 @@
+//
+//  Column.m
+//  Abacus
+//
+//  Created by Anil K Nellutla on 3/24/14.
+//  Copyright (c) 2014 Anil K Nellutla. All rights reserved.
+//
+
+#import "Column.h"
+
+@implementation Column
+
+@end
